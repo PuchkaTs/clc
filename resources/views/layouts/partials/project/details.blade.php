43b95b0d91a224a6bef0bcb2033ad0ed0d9f1cb1
unknown <ncst.puje@gmail.com>
2015-06-19 19:27:10 +0900

    changed bedrooms

--- resources/views/layouts/partials/project/details.blade.php
+++ resources/views/layouts/partials/project/details.blade.php
@@ -2,7 +2,7 @@
     <p><i class="fa fa-flag" style="width: 15px;"></i> {{ $project->available ? 'Available' : 'Rented' }}</p>
 </div>
 <div class="col-md-6">
-    <p><i class="fa fa-cube" style="width: 15px;"></i> ROOMS:  {{ $project->rooms}} </p>
+    <p><i class="fa fa-cube" style="width: 15px;"></i> BEDROOMS:  {{ $project->rooms}} </p>
 </div>
 <div class="col-md-6">
     <p><i class="fa fa-leaf" style="width: 15px;"></i> BATHROOMS:  {{ $project->bathrooms}}</p>
